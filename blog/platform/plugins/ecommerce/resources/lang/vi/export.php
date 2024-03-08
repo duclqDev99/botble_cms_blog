cd273708b1ee703ed4bf5ba71aeb2ade3e194cec
duclqDev99 <141119092+duclqDev99@users.noreply.github.com>
2024-03-08 15:49:10 +0700

    plugin ecommerce

---
+++ blog/platform/plugins/ecommerce/resources/lang/vi/export.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'products' => [
+        'name' => 'Xuất khẩu sản phẩm',
+        'title' => 'Xuất khẩu Sản phẩm (CSV)',
+        'total_products' => 'Tổng số sản phẩm',
+        'total_variations' => 'Tổng số biến thể',
+    ],
+    'start_export' => 'Bắt đầu xuất khẩu',
+    'export' => 'Xuất khẩu',
+];
+
